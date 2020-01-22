e2cf63d2364f19e8270543e4a5b5159bca71459f
TakGlobus <tkurihana@uchicago.edu>
2020-01-22 10:50:38 -0600

    algorithm initial push

---
+++ algorithm/hw1/src/student_code_1.h
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to
+//    1. Read the programming assignment in homework #1.
+//    2. Implement function GetStudentName.
+//    3. Implement function FindOptimalSubsequenceValue.
+//    4. Compile your code as explained in the PDF file.
+//    5. Run the executable.
+//    6. Test and debug your code.
+//    7. Submit your code ("student_code_1.h") and results ("solution_1.dat")
+//       via Canvas.
+///////////////////////////////////////////////////////////////////////////////
+
+//required libraries
+#include <string>
+#include <vector>
+
+//you can include standard C++ libraries here
+#include <queue>
+
+// This function should return your name.
+// The name should match your name in Canvas
+
+void GetStudentName(std::string& your_name)
+{
+   //replace the placeholders "Firstname" and "Lastname"
+   //with you first name and last name
+   your_name.assign("Firstname Lastname");
+}
+
+int FindOptimalSubsequenceValue(const std::vector<int>&  a)
+{
+
+   return 0 /* your answer */;
+}
