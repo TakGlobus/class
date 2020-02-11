ed88ff7de745b3063977df5dc950558931510167
TakGlobus <tkurihana@uchicago.edu>
2020-02-11 13:09:32 -0600

    save 02/10 tmp code

--- algorithm/hw2/dev/hw2_test1.py
+++ algorithm/hw2/dev/hw2_test1.py
@@ -9,9 +9,12 @@ import numpy as np
 #s = "aaadbbbba"
 #t = "aaacbbbbc"
 
-correct_answer=4
-s='elephant'
-t='elephantzzt'
+#correct_answer=4
+#s='elephant'
+#t='elephantt'
+correct_answer=3
+s='eleph'
+t='elephphhhhhhhhhp'
 
 #correct_answer = 14
 #s = "sjpgzirqwuuhzttfxtqetsfncggealzdqwmxpyisulygnmmtfhzzkkiumvduocip"
@@ -32,39 +35,19 @@ def my_solution(x,y):
   ny = len(y)
   dp = np.zeros((nx+1, ny+1))
   dp_s = []
-  update = False
   for ix in range(1,nx+1,1):
+    updated = False
     for iy in range(1,ny+1,1):
         i = ix-1
         j = iy-1
         if x[i] == y[j] and x[i-1] != y[j-1]:
-          if not update:
-            dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1]+1)
-            update = True
-            print("tmp dp max {} | i = {} j = {}".format(dp[ix,iy], i,j))
-          else:
-            dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1])
-            update = False
+              dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1]+1)
+              #print("tmp dp max {} | i = {} j = {}".format(dp[ix,iy], i,j))
         elif x[i] == y[j] and x[i-1] == y[j-1]:
-          if not update:
-            dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-2,iy-2]+1)
-            update=True
-          else:
-            dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-2,iy-2])
-            update = False
-            
-          #dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy])
-          #print("tmp dp max {} | i = {} j = {}".format(dp[ix,iy], i,j))
+              dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-2,iy-2]+1)
         else:
           dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1])
-          update=False
            
-        #if i == 38 and j == 3:
-        #  print(x[i-2:i+1], y[j-2:j+1])
-        #  print(dp[i,:j+1])
-        #  print(max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1]+1))
-        #  print(max(dp[ix,iy-1], dp[ix-1,iy],dp[ix,iy-2], dp[ix-2,iy], dp[ix-2,iy-2]+1))
-        #  stop
 
   etime = time.perf_counter()
   print(" My Answer = {}  | Correct Answer = {} ".format(np.max(dp), correct_answer))
@@ -76,16 +59,3 @@ def my_solution(x,y):
 if __name__ == "__main__":
   my_solution(s,t)
 
-#    for iy in range(1,ny+1,1):
-#        i = ix-1
-#        j = iy-1
-#        if x[i] == y[j]:
-#          if x[i-1] != y[j-1]:
-#            dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1]+1)
-#          else:
-#            if x[i-2] == y[j-2]:
-#              dp[ix,iy] = max(dp[ix,iy-2], dp[ix-2,iy], dp[ix-2,iy-2]+1)
-#            else:
-#              dp[ix,iy] = max(dp[ix,iy-2], dp[ix-2,iy], dp[ix-2,iy-2])
-#        else:
-#          dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy])
