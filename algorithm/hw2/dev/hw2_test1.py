29dcf695967b07247fd1aa456dc88987a3ff9971
TakGlobus <tkurihana@uchicago.edu>
2020-02-11 14:29:11 -0600

    02/11 maybe correct answer?

--- algorithm/hw2/dev/hw2_test1.py
+++ algorithm/hw2/dev/hw2_test1.py
@@ -9,20 +9,23 @@ import numpy as np
 #s = "aaadbbbba"
 #t = "aaacbbbbc"
 
+#correct_answer=2
+#s='elep'
+#t='elepppp'
 #correct_answer=4
 #s='elephant'
 #t='elephantt'
-correct_answer=3
-s='eleph'
-t='elephphhhhhhhhhp'
+#correct_answer=3
+#s='eleph'
+#t='elephphhhhhhhhhp'
 
 #correct_answer = 14
 #s = "sjpgzirqwuuhzttfxtqetsfncggealzdqwmxpyisulygnmmtfhzzkkiumvduocip"
 #t = "naqihutusctsisjmziykrazehzymsocxoutwiszoabcxwgarfygduybidnzntfu"
 
-#correct_answer= 11
-#s="oijjgveresojcjmolkwmdicrhlcjafbifaxuweigdtrohkatddtzhgaemclwowgfiikxtwn"
-#t="padztmkzdmwhmznfnhwfajaalirnwcuefwjkibual"
+correct_answer= 11
+s="oijjgveresojcjmolkwmdicrhlcjafbifaxuweigdtrohkatddtzhgaemclwowgfiikxtwn"
+t="padztmkzdmwhmznfnhwfajaalirnwcuefwjkibual"
 
 def my_solution(x,y):
   """
@@ -36,18 +39,24 @@ def my_solution(x,y):
   dp = np.zeros((nx+1, ny+1))
   dp_s = []
   for ix in range(1,nx+1,1):
-    updated = False
     for iy in range(1,ny+1,1):
-        i = ix-1
-        j = iy-1
-        if x[i] == y[j] and x[i-1] != y[j-1]:
-              dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1]+1)
-              #print("tmp dp max {} | i = {} j = {}".format(dp[ix,iy], i,j))
-        elif x[i] == y[j] and x[i-1] == y[j-1]:
-              dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-2,iy-2]+1)
-        else:
-          dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1])
-           
+       i = ix-1
+       j = iy-1
+       if x[i] == y[j] and x[i-1] != y[j-1]:
+         if dp[ix-1,iy-1] - dp[ix-2,iy-2] == 0:
+           dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1]+1)
+           print("tmp dp max {} | i = {} j = {}".format(dp[ix,iy], i,j))
+         else:
+           dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1])  
+       elif x[i] == y[j] and x[i-1] == y[j-1]:
+         if dp[ix-1,iy-1] - dp[ix-2,iy-2] == 0:
+           dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-2,iy-2]+1)
+         else:
+           dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1])  
+       else:
+         dp[ix,iy] = max(dp[ix,iy-1], dp[ix-1,iy], dp[ix-1,iy-1])
+
+      
 
   etime = time.perf_counter()
   print(" My Answer = {}  | Correct Answer = {} ".format(np.max(dp), correct_answer))
