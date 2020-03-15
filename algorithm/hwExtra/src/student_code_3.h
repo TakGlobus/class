553418793aeb76c0c457094eeb284c281d72486c
TakGlobus <tkurihana@uchicago.edu>
2020-03-15 14:33:01 -0500

    push original codes

---
+++ algorithm/hwExtra/src/student_code_3.h
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to
+//    1. Read the programming assignment in homework #3.
+//    2. Implement function GetStudentName.
+//    3. Implement function FindOptimalColoringWeight.
+//    4. Compile your code as explained in the PDF file.
+//    5. Run the executable.
+//    6. Test and debug your code.
+//    7. Submit your code ("student_code_3.h") and results ("solution_3.dat")
+//       via Canvas.
+///////////////////////////////////////////////////////////////////////////////
+
+//required libraries
+#include <string>
+#include <vector>
+
+//you can include standard C++ libraries here
+#include <queue>
+
+// This function should return your name.
+// The name should match your name in Canvas
+
+void GetStudentName(std::string& your_name)
+{
+   //replace the placeholders "Firstname" and "Lastname"
+   //with you first name and last name
+   your_name.assign("Firstname Lastname");
+}
+
+int FindOptimalColoringWeight(const std::vector<int>&  r, const std::vector<int>&  g, const std::vector<int>&  b)
+{
+
+   return 0 /* your answer */;
+}
